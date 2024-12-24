c13ff445b39daa105e9b6b4fd25501dd4b305447
Batiazinga <luc.nguyen-the@pm.me>
2024-12-24 15:54:59 +0100

    Improve Wishart plot

--- plot_wishart.py
+++ plot_wishart.py
@@ -1,6 +1,8 @@
+import math
 from pathlib import Path
 
-import plotly.express as px
+import numpy as np
+import plotly.graph_objects as go
 
 
 def read_eigen_values(filename: str) -> tuple[int, int, tuple[float, ...]]:
@@ -13,6 +15,34 @@ def read_eigen_values(filename: str) -> tuple[int, int, tuple[float, ...]]:
     return n, p, tuple(eigen_values)
 
 
+def marchenko_pastur(n: int, p: int, x: np.ndarray) -> np.ndarray:
+    c = float(p) / float(n)
+    lambda_p = (1 + math.sqrt(c)) ** 2
+    lambda_m = (1 - math.sqrt(c)) ** 2
+    norm = 2 * math.pi * c
+    return np.divide(
+        np.divide(np.sqrt(np.maximum((lambda_p - x) * (x - lambda_m), 0.0)), x), norm
+    )
+
+
+fig = go.Figure()
+
 n, p, eigen_values = read_eigen_values("wishart_n_p_eigs.txt")
-fig = px.histogram(eigen_values)
+empirical_density = np.histogram(eigen_values, bins=30)
+empirical_delta = empirical_density[1][1] - empirical_density[1][0]
+fig.add_trace(
+    go.Bar(x=empirical_density[1], y=empirical_density[0] / float(p) / empirical_delta)
+)
+
+x = np.arange(0.01, 1.6, 0.01)
+limit_density = marchenko_pastur(n, p, x)
+fig.add_trace(
+    go.Scatter(
+        {
+            "x": x,
+            "y": limit_density,
+        },
+    )
+)
+
 fig.show()
