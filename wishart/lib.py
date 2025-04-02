b285142cc5ffabd118258c53f60d26b18ba9de7b
Batiazinga <luc.nguyen-the@pm.me>
2025-04-02 21:10:06 +0200

    Make code more idiomatic

--- wishart/lib.py
+++ wishart/lib.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def scm(data: np.ndarray) -> np.ndarray:
     """Sample Covariance Matrix."""
-    return np.matmul(data, np.transpose(data)) / data.shape[1]
+    return data @ data.T / data.shape[1]
 
 
 def marchenko_pastur(c: float, x: np.ndarray) -> np.ndarray:
@@ -15,9 +15,7 @@ def marchenko_pastur(c: float, x: np.ndarray) -> np.ndarray:
     """
     lower, upper = marchenko_pastur_bounds(c)
     norm = 2 * math.pi * c
-    return np.divide(
-        np.divide(np.sqrt(np.maximum((upper - x) * (x - lower), 0.0)), x), norm
-    )
+    return np.sqrt(np.maximum((upper - x) * (x - lower), 0.0)) / x / norm
 
 
 def marchenko_pastur_bounds(c: float) -> tuple[float, float]:
@@ -29,28 +27,24 @@ def limit_stieltjes(c: float, z: np.ndarray) -> np.ndarray:
     lower, upper = ((1 - math.sqrt(c)) ** 2, (1 + math.sqrt(c)) ** 2)
     num = 1 - c - z + np.emath.sqrt(z - lower) * np.emath.sqrt(z - upper)
     denom = 2 * c * z
-    return np.divide(num, denom)
+    return num / denom
 
 
 def inverse_limit_co_stieltjes(c: float, z: np.ndarray) -> np.ndarray:
-    """Functional inverse of the Stieltjes transform of the Sample Covariance Matrix of
-    the observations.
-    """
-    return -np.divide(1.0, z) + np.divide(c, 1.0 + z)
+    """Functional inverse of the Stieltjes transform of the Gram Matrix."""
+    return -1.0 / z + c / (1.0 + z)
 
 
 def inverse_limit_stieltjes(c: float, z: np.ndarray) -> np.ndarray:
-    """Functional inverse of the Stieltjes transform of the Sample Covariance Matrix of
-    the variables (~blue transform).
+    """Functional inverse of the Stieltjes transform of the Sample Covariance Matrix
+    (~blue transform).
     """
-    return -np.divide(1.0, z) + np.divide(1.0, 1.0 + c * z)
+    return -1.0 / z + 1.0 / (1.0 + c * z)
 
 
 def gamma(c: float, z: np.ndarray) -> np.ndarray:
-    """Related to the Stieltjes of the Sample Covariance Matrix of the observations by:
-    gamma = - 1 / stieljes
-    """
+    """Related to the Stieltjes of the Gram Matrix by gamma = - 1 / stieljes"""
     lower, upper = ((1 - math.sqrt(c)) ** 2, (1 + math.sqrt(c)) ** 2)
     num = 2 * z
     denom = 1 - c + z - np.emath.sqrt(z - lower) * np.emath.sqrt(z - upper)
-    return np.divide(num, denom)
+    return num / denom
